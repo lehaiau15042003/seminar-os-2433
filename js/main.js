3b6ae950ec48064edb562af7cc76c9c82a3ed7bb
Lê Hải Âu <lehaiau15042003@gmail.com>
2025-05-27 00:21:14 +0700

    deleted trash file

--- js/main.js
+++ js/main.js
@@ -5,8 +5,9 @@ import LRU from './algorithms/PageReplacement/LRU.js'
 import OPTIMAL from './algorithms/PageReplacement/OPTIMAL.js'
 import CLOCK from './algorithms/PageReplacement/CLOCK.js'
 
-let pages = [7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1];
+//let pages = [7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1];
 //console.log(FIFO(pages, 3));
 //console.log(LRU(pages, 3));
 //console.log(OPTIMAL(pages, 3));
-console.log(CLOCK(pages, 3));
+//console.log(CLOCK(pages, 3));
+
