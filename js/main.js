4d043679ea3950c56aaab24a1d3b55fc9922fad8
Lê Hải Âu <lehaiau15042003@gmail.com>
2025-05-27 21:06:11 +0700

    Resolve merge conflicts

--- js/main.js
+++ js/main.js
@@ -11,3 +11,68 @@ import CLOCK from './algorithms/PageReplacement/CLOCK.js'
 //console.log(OPTIMAL(pages, 3));
 //console.log(CLOCK(pages, 3));
 
+let pages = [];
+let frame = [];
+window.onload = function() {
+    const pageInput = document.querySelector('.pageInput');
+    const pageDisplay = document.querySelector('.card');
+    const frameSize = document.querySelector('.frameInput');
+    const algorithms = document.querySelector('.pageReplacement').value;
+    const frameDisplay = document.querySelector('.frame');
+
+    pageInput.addEventListener('input', (event) => {
+            const value = parseInt(pageInput.value, 10);
+            if(!isNaN(value)){
+                pages.push(value);
+                renderPages();
+                pageInput.value = '';
+            }
+
+            function renderPages(){
+                pageDisplay.innerHTML = '';
+                pages.forEach(p => {
+                    const span = document.createElement('span');
+                    span.className = 'page-box';
+                    span.innerText = p;
+                    pageDisplay.appendChild(span);
+                });
+            }
+    });
+
+    frameSize.addEventListener('input', (event) => {
+        const value = parseInt(frameSize.value, 10);
+        if(!isNaN(value)){
+            frame.push(value);
+            renderFrame(count);
+            frameSize.value = '';
+        }
+
+        function renderFrame(count) {
+            frame.innerHTML = '';
+            for(let i=0; i < count; i++){
+                const div = document.createElement('div');
+                div.className = 'frame-rows';
+                div.innerText = `Frame ${i + 1}`;
+                frame.appendChild(div);
+            }
+        }
+    });
+
+    let res;
+    switch(algorithms) {
+        case "FIFO":
+            res = FIFO(pages, frameSize);
+            break;
+        case "LRU":
+            res = FIFO(pages, frameSize);
+            break;
+        case "OPTIMAL":
+            res = OPTIMAL(pages, frameSize);
+            break;
+        case "CLOCK":
+            res = CLOCK(pages, frameSize);
+            break;
+        default:
+            res = "Thuật toán không hợp lệ";
+    }
+}
